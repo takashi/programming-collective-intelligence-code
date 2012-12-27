812a98d95e19d28ef11679537bccf0b34a10292e
David R. Pugh <drobert.pugh@gmail.com>
2012-12-27 12:06:13 +0000

    Added docstrings and corrected small typos.

--- chapter8/numpredict.py
+++ chapter8/numpredict.py
@@ -1,149 +1,298 @@
-from random import random,randint
+import random
 import math
 
-def wineprice(rating,age):
-  peak_age=rating-50
+def winePrice(rating, age):
+    """Articifical wine price model.
+
+    Keyword arguments:
+        rating -- float, a wine's rating
+        age    -- int, the current age of a wine
+
+    """
+    peak_age = rating - 50
   
-  # Calculate price based on rating
-  price=rating/2
-  if age>peak_age:
-    # Past its peak, goes bad in 10 years
-    price=price*(5-(age-peak_age)/2)
-  else:
-    # Increases to 5x original value as it
-    # approaches its peak
-    price=price*(5*((age+1)/peak_age))
-  if price<0: price=0
-  return price
+    # Calculate price based on rating
+    price = rating / 2.0
 
-
-def wineset1():
-  rows=[]
-  for i in range(300):
-    # Create a random age and rating
-    rating=random()*50+50
-    age=random()*50
-
-    # Get reference price
-    price=wineprice(rating,age)
-    
-    # Add some noise
-    price*=(random()*0.2+0.9)
-
-    # Add to the dataset
-    rows.append({'input':(rating,age),
-                 'result':price})
-  return rows
-
-def euclidean(v1,v2):
-  d=0.0
-  for i in range(len(v1)):
-    d+=(v1[i]-v2[i])**2
-  return math.sqrt(d)
-
-
-def getdistances(data,vec1):
-  distancelist=[]
-  
-  # Loop over every item in the dataset
-  for i in range(len(data)):
-    vec2=data[i]['input']
-    
-    # Add the distance and the index
-    distancelist.append((euclidean(vec1,vec2),i))
-  
-  # Sort by distance
-  distancelist.sort()
-  return distancelist
-
-def knnestimate(data,vec1,k=5):
-  # Get sorted distances
-  dlist=getdistances(data,vec1)
-  avg=0.0
-  
-  # Take the average of the top k results
-  for i in range(k):
-    idx=dlist[i][1]
-    avg+=data[idx]['result']
-  avg=avg/k
-  return avg
-
-def inverseweight(dist,num=1.0,const=0.1):
-  return num/(dist+const)
-
-def subtractweight(dist,const=1.0):
-  if dist>const: 
-    return 0
-  else: 
-    return const-dist
-
-def gaussian(dist,sigma=5.0):
-  return math.e**(-dist**2/(2*sigma**2))
-
-def weightedknn(data,vec1,k=5,weightf=gaussian):
-  # Get distances
-  dlist=getdistances(data,vec1)
-  avg=0.0
-  totalweight=0.0
-  
-  # Get weighted average
-  for i in range(k):
-    dist=dlist[i][0]
-    idx=dlist[i][1]
-    weight=weightf(dist)
-    avg+=weight*data[idx]['result']
-    totalweight+=weight
-  if totalweight==0: return 0
-  avg=avg/totalweight
-  return avg
-
-def dividedata(data,test=0.05):
-  trainset=[]
-  testset=[]
-  for row in data:
-    if random()<test:
-      testset.append(row)
+    if age > peak_age:
+        # Past its peak, goes bad in 10 years
+        price = price * (5 - (age - peak_age) / 2.0)
     else:
-      trainset.append(row)
-  return trainset,testset
+        # Increases to 5x original value as it approaches its peak
+        price = price * ( 5 * ((age + 1) / peak_age))
 
-def testalgorithm(algf,trainset,testset):
-  error=0.0
-  for row in testset:
-    guess=algf(trainset,row['input'])
-    error+=(row['result']-guess)**2
-    #print row['result'],guess
-  #print error/len(testset)
-  return error/len(testset)
+    if price < 0:
+        # Can't have negative prices!
+        price = 0
 
-def crossvalidate(algf,data,trials=100,test=0.1):
-  error=0.0
-  for i in range(trials):
-    trainset,testset=dividedata(data,test)
-    error+=testalgorithm(algf,trainset,testset)
-  return error/trials
+    return price
 
-def wineset2():
-  rows=[]
-  for i in range(300):
-    rating=random()*50+50
-    age=random()*50
-    aisle=float(randint(1,20))
-    bottlesize=[375.0,750.0,1500.0][randint(0,2)]
-    price=wineprice(rating,age)
-    price*=(bottlesize/750)
-    price*=(random()*0.2+0.9)
-    rows.append({'input':(rating,age,aisle,bottlesize),
-                 'result':price})
-  return rows
+
+def wineSet1(seed):
+    """Artificial wine price data set.
+
+    Keyword args:
+        seed -- int, seed for random number generator
+
+    """
+    random.seed(seed)
+    rows = []
+    
+    for i in range(300):
+        # Create a random age and rating
+        rating = random.random() * 50 + 50
+        age    = random.random() * 50
+
+        # Get reference price
+        price  = wineprice(rating,age)
+    
+        # Add some noise
+        price  *= (random.random() * 0.2 + 0.9)
+
+        # Add to the dataset
+        rows.append({'input':(rating,age), 'result':price})
+
+    return rows
+
+def euclidean(v1, v2):
+    """Euclidean distance between two vectors.
+
+    Keyword arguments:
+        v1 -- vector of floats
+        v2 -- vector of floats
+
+    """
+    d = 0.0
+    for i in range(len(v1)):
+        d += (v1[i] - v2[i])**2
+
+    return math.sqrt(d)
+
+
+def getDistances(data, vec1):
+    """Computes the similarity between vec1 and every other vector in data.
+    Currently, similarity is measured by the Euclidean distance between vectors
+    of item attributes.
+    
+    Keyword arguments:
+        data -- data set containing items and their attributes
+        vec1 -- vector of attributes for item of interest
+
+    """
+    distancelist = []
+  
+    # Loop over every item in the dataset
+    for i in range(len(data)):
+        vec2 = data[i]['input']
+    
+        # Add the distance and the index
+        distancelist.append((euclidean(vec1,vec2),i))
+  
+    # Sort (in-place!) by distance
+    distancelist.sort()
+
+    return distancelist
+
+def knnEstimate(data, vec1, k=5):
+    """Computes the k-NN for a item from data with a vector vec1 of attributes.
+
+    Keyword arguments:
+        data -- data set containing items and their attributes
+        vec1 -- vector of attributes for item of interest
+        k    -- number of nearest neighbors to average over
+
+    """
+    # Get sorted distances
+    dlist = getDistances(data, vec1)
+    avg   = 0.0
+  
+    # Take the average of the top k results
+    for i in range(k):
+        idx = dlist[i][1]
+        avg += data[idx]['result']
+    avg = avg / float(k)
+
+    return avg
+
+def inverseWeight(dist, num=1.0, const=0.1):
+    """ Weighting function for neighbors.
+
+    A neighbor is weighted inversely proportional to its distance from the item
+    of interest.
+
+    Keyword arguments:
+        dist  -- float, distance between an item and the item of interest
+        num   -- float, parameter to tweak
+        const -- float, need to add a small amount to each distance to prevent
+                 some items receiveing extremely large weights.
+
+    """
+    return num / (dist + const)
+
+def subtractWeight(dist, const=1.0):
+    """ Weighting function for neighbors.
+
+    The distance between a neighbor and the item of interest is subtracted
+    from a constant.
+
+    Keyword arguments:
+        dist  -- float, distance between an item and the item of interest
+        const -- float, constant from which the distances between items
+                 are subtracted.
+
+    """
+    if dist > const: 
+        return 0
+    else: 
+        return const - dist
+
+def gaussianWeight(dist, sigma=5.0):
+    """ Weighting function for neighbors.
+
+    A neighbor is weighted using a Gaussian function.
+
+    Keyword arguments:
+        dist  -- float, distance between an item and the item of interest
+        sigma -- float, controls how fast the weights decline with distance.
+                 Neighbor weights will decline more quickly for low values
+                 of sigma.
+
+    """
+    return math.e**(-dist**2 / (2 * sigma**2))
+
+def knnWeightedEstimate(data, vec1, k=5, weightf=gaussianWeight):
+    """Computes the weighted k-NN for a item from data with a vector vec1 of
+    attributes.
+
+    Keyword arguments:
+        data    -- data set containing items and their attributes
+        vec1    -- vector of attributes for item of interest
+        k       -- number of nearest neighbors to average over
+        weightf -- weighting function
+        
+    """
+    # Get distances
+    dlist=getDistances(data, vec1)
+    avg=0.0
+    totalweight=0.0
+  
+    # Get weighted average
+    for i in range(k):
+        dist        = dlist[i][0]
+        idx         = dlist[i][1]
+        weight      = weightf(dist)
+        avg         += weight * data[idx]['result']
+        totalweight += weight
+        
+    # catch degenerate case!
+    if totalweight==0:
+        return 0
+    
+    # compute the weighted average
+    avg = avg / totalweight
+
+    return avg
+
+def divideData(data, test=0.05):
+    """ Divides data into training and testing sets.
+
+    Keyword arguments:
+        data -- data set containing items and their attributes
+        test -- fraction of data to be included in the test set
+
+    """
+    trainset=[]
+    testset=[]
+
+    # divide data randomly
+    for row in data:
+        if random.random() < test:
+            testset.append(row)
+        else:
+            trainset.append(row)
+
+    return trainset, testset
+
+def testAlgorithm(algf, trainset, testset):
+    """ Tests a given algorithm by predicting a values in testset by applying
+    the algf to the data in trainset.  Function returns the average mean-square
+    error (MSE) of algf.
+
+    Keyword arguments:
+        algf     -- algorithm/function to be evaluated
+        trainset -- training data
+        testset  -- testing data
+
+    """
+    error = 0.0
+
+    # loop over each row in testset  
+    for row in testset:
+        # predict the result using trainset
+        guess = algf(trainset, row['input'])
+        # compute the squared error
+        error += (row['result'] - guess)**2
+
+    # compute the MSE
+    MSE = error / len(testset)
+
+    return MSE
+
+def crossValidate(algf, data, trials=100, test=0.05):
+    """Performs cross-validation of an algorithm on a data set. Specifically,
+    function returns the average MSE of an algorithm over a number of trials.
+
+    Keyword arguments:
+        algf   -- algorithm/function to be evaluated
+        data   -- data set containing items and their attributes
+        trials -- number of times to call testAlgorithm
+        test   -- fraction of data to be included in the test set
+
+    """
+    error = 0.0
+    for i in range(trials):
+        trainset, testset = divideData(data, test)
+        error += testAlgorithm(algf, trainset, testset)
+    return error / trials
+
+def wineSet2(seed):
+    """Artificial wine price data set.
+
+    Keyword args:
+        seed -- int, seed for random number generator
+
+    """
+    random.seed(seed)
+    rows=[]
+
+    # generate the various attributes that determine bottle price
+    for i in range(300):
+        rating     = random.random() * 50 + 50
+        age        = random.random() * 50
+        aisle      = float(random.randint(1, 20))
+        bottlesize = [375.0, 750.0, 1500.0, 3000.0][random.randint(0, 3)]
+        price      = wineprice(rating, age)
+        price      *= (bottlesize / 750)
+        price      *= (random.random() * 0.9 + 0.2)
+        rows.append({'input':(rating,age,aisle,bottlesize), 'result':price})
+
+    return rows
 
 def rescale(data,scale):
-  scaleddata=[]
-  for row in data:
-    scaled=[scale[i]*row['input'][i] for i in range(len(scale))]
-    scaleddata.append({'input':scaled,'result':row['result']})
-  return scaleddata
+    """Re-scales data attributes.
 
+    Keywords arguments:
+        data  -- data set containing items and their attributes
+        scale -- vector of floats which are used to rescale variables (should
+                 have same length as number of attributes).
+
+    """
+    scaleddata = []
+    for row in data:
+        scaled = [scale[i] * row['input'][i] for i in range(len(scale))]
+        scaleddata.append({'input':scaled,'result':row['result']})
+    return scaleddata
+"""
 def createcostfunction(algf,data):
   def costf(scale):
     sdata=rescale(data,scale)
@@ -210,3 +359,4 @@ def probabilitygraph(data,vec1,high,k=5,weightf=gaussian,ss=5.0):
     
   plot(t1,smoothed)
   show()
+"""
