831af0eb26bdcded3c87e78d3a3f027c4f76dd22
David R. Pugh <drobert.pugh@gmail.com>
2012-12-28 19:15:25 +0000

    Updated wineSet3() and probGuess functions.

--- chapter8/numpredict.py
+++ chapter8/numpredict.py
@@ -9,17 +9,18 @@ def winePrice(rating, age):
         age    -- int, the current age of a wine
 
     """
+    # peak age is older for "good" wine and almost immediate for "bad" wine
     peak_age = rating - 50
   
     # Calculate price based on rating
     price = rating / 2.0
 
     if age > peak_age:
-        # Past its peak, goes bad in 5 years
-        price = price * (5 - (age - peak_age))
+        # Past its peak, goes bad in 10 years
+        price = price * (5 - 0.5 * (age - peak_age))
     else:
         # Increases to 5x original value as it approaches its peak
-        price = price * ( 5 * ((age + 1) / peak_age))
+        price = price * ( 5 * (age / peak_age))
 
     if price < 0:
         # Can't have negative prices!
@@ -38,16 +39,16 @@ def wineSet1(seed):
     random.seed(seed)
     rows = []
     
-    for i in range(300):
+    for i in range(200):
         # Create a random age and rating
-        rating = random.random() * 50 + 50
-        age    = random.random() * 50
+        rating = random.random() * 50 + 50 # rating is U ~ [50, 100] 
+        age    = random.random() * 50 # rating is U ~ [0, 50]
 
-        # Get reference price
+        # Get fundamental value
         price  = winePrice(rating,age)
     
-        # Add some noise
-        price  *= (random.random() * 0.4 + 0.8)
+        # Add some noise 
+        price  *= (random.random() * 0.4 + 0.8) # U ~ [0.8, 1.2]
 
         # Add to the dataset
         rows.append({'input':(rating ,age), 'result':price})
@@ -146,7 +147,7 @@ def subtractWeight(dist, const=1.0):
     else: 
         return const - dist
 
-def gaussianWeight(dist, sigma=10.0):
+def gaussianWeight(dist, sigma=1.0):
     """ Weighting function for neighbors.
 
     A neighbor is weighted using a Gaussian function.
@@ -215,8 +216,8 @@ def divideData(data, test=0.05):
 
 def testAlgorithm(algf, trainset, testset):
     """ Tests a given algorithm by predicting a values in testset by applying
-    the algf to the data in trainset.  Function returns the average mean-square
-    error (MSE) of algf.
+    the algf to the data in trainset.  Function returns the mean-square error
+    (MSE) of algf.
 
     Keyword arguments:
         algf     -- algorithm/function to be evaluated
@@ -256,7 +257,9 @@ def crossValidate(algf, data, trials=100, test=0.05):
     return error / trials
 
 def wineSet2(seed):
-    """Artificial wine price data set.
+    """Artificial wine price data set. Adds two variables: aisle, which has no
+    impact on price, and bottlesize which does impact the price of a bottle but
+    has a very different scale than rating and age variables.
 
     Keyword args:
         seed -- int, seed for random number generator
@@ -266,14 +269,14 @@ def wineSet2(seed):
     rows=[]
 
     # generate the various attributes that determine bottle price
-    for i in range(300):
+    for i in range(200):
         rating     = random.random() * 50 + 50
-        age        = random.random() * 50
+        age        = random.random() * 50 
         aisle      = float(random.randint(1, 20))
-        bottlesize = [375.0, 750.0, 1500.0, 3000.0][random.randint(0, 3)]
+        bottlesize = [375.0, 750.0, 1500.0][random.randint(0, 2)]
         price      = winePrice(rating, age)
         price      *= (bottlesize / 750.0)
-        price      *= (random.random() * 0.9 + 0.2)
+        price      *= (random.random() * 0.2 + 0.9) # U ~ [0.9, 1.1]
         rows.append({'input':(rating, age, aisle, bottlesize), 'result':price})
 
     return rows
@@ -294,7 +297,7 @@ def rescale(data,scale):
     return scaleddata
 
 def createCostFunction(algf, data):
-    """Wrapper function used to optimize crossValidate.
+    """Wrapper function used to optimize crossValidate by choosing the scale.
 
     Keyword arguments:
         algf   -- algorithm/function to be evaluated
@@ -315,38 +318,64 @@ def createCostFunction(algf, data):
     
     return costf
 
-"""
-weightdomain=[(0,10)]*4
+# Domain over which to search for optimal weights (assumes that weights cannot exceed 20!)
+weightdomain = [(0, 20)] * 4
 
-def wineset3():
-  rows=wineset1()
-  for row in rows:
-    if random()<0.5:
-      # Wine was bought at a discount store
-      row['result']*=0.6
-  return rows
+def wineSet3(seed):
+    """Artificial wine price data set. Reverts to wineSet1 variables, then
+    captures the idea that wine is purchased by two different groups of people:
+    those who purchase wine at liquor stores and those that purchase wine at
+    discount stores. 
 
-def probguess(data,vec1,low,high,k=5,weightf=gaussian):
-  dlist=getdistances(data,vec1)
-  nweight=0.0
-  tweight=0.0
-  
-  for i in range(k):
-    dist=dlist[i][0]
-    idx=dlist[i][1]
-    weight=weightf(dist)
-    v=data[idx]['result']
+    Keyword args:
+        seed -- int, seed for random number generator
+
+    """
+    rows = wineSet1(seed)
+
+    for row in rows:
+        # Wine was bought at a discount store (this variable is unobserved!)
+        if random.random() < 0.5:
+            row['result'] *= 0.6
+    return rows
+
+def probguess(data, vec1, low, high, k=5, weightf=gaussian):
+    """Function returns a value between 0 and 1 representing the probability
+    that the price of an item with attributes vec1 lies between low and high.
+
+    Keyword arugments:
+        data    -- data set containing items and their attributes
+        vec1    -- vector of attributes describing the item of interest
+        low     -- lower bound on the price range
+        high    -- upper bound on the price range
+        k       -- number of neighbors to use in the calculation
+        weightf -- the weighting function to apply to the raw distance measures
+
+    """
+    dlist   = getDistances(data, vec1)
+    nweight = 0.0
+    tweight = 0.0
+
+    for i in range(k):
+        dist   = dlist[i][0]
+        idx    = dlist[i][1]
+        weight = weightf(dist)
+        v      = data[idx]['result']
     
-    # Is this point in the range?
-    if v>=low and v<=high:
-      nweight+=weight
-    tweight+=weight
-  if tweight==0: return 0
-  
-  # The probability is the weights in the range
-  # divided by all the weights
-  return nweight/tweight
+        # Is this point in the range?
+        if v >= low and v <= high:
+            nweight += weight
 
+        tweight += weight
+
+    # Catch a degenerate case (don't think this could happen with most weightf)
+    if tweight == 0:
+        return 0
+  
+  # The probability is the weights in the range divided by all the weights
+  return nweight / tweight
+
+"""
 from pylab import *
 
 def cumulativegraph(data,vec1,high,k=5,weightf=gaussian):
