760f591d13f7e4cc51b1b0654e8a812fdf53afd5
David R. Pugh <drobert.pugh@gmail.com>
2013-01-11 15:09:14 +0000

    Complete re-write of newsfeatures.py and nnmf.py

--- chapter10/newsfeatures.py
+++ chapter10/newsfeatures.py
@@ -1,141 +1,242 @@
+import numpy as np
 import feedparser
 import re
 
+feedlist=['http://feeds.reuters.com/reuters/businessNews',
+          'http://feeds.reuters.com/reuters/companyNews',
+          'http://feeds.reuters.com/news/wealth',
+          'http://feeds.reuters.com/reuters/bankruptcyNews',
+          'http://feeds.reuters.com/reuters/bondsNews',
+          'http://feeds.reuters.com/news/deals',
+          'http://feeds.reuters.com/news/economy',
+          'http://feeds.reuters.com/reuters/financialServicesrealEstateNews',
+          'http://feeds.reuters.com/reuters/hotStocksNews',
+          'http://feeds.reuters.com/reuters/newIssuesNews',
+          'http://feeds.reuters.com/reuters/mergersNews',
+          'http://feeds.reuters.com/reuters/privateequityNews',
+          'http://feeds.reuters.com/reuters/governmentfilingsNews',
+          'http://feeds.reuters.com/reuters/smallBusinessNews',
+          'http://feeds.reuters.com/reuters/summitNews',
+          'http://feeds.reuters.com/reuters/USdollarreportNews',
+          'http://feeds.reuters.com/news/usmarkets',
+          'http://feeds.reuters.com/reuters/basicmaterialsNews',
+          'http://feeds.reuters.com/reuters/cyclicalconsumergoodsNews',
+          'http://feeds.reuters.com/reuters/USenergyNews',
+          'http://feeds.reuters.com/reuters/environment',
+          'http://feeds.reuters.com/reuters/financialsNews',
+          'http://feeds.reuters.com/reuters/UShealthcareNews',
+          'http://feeds.reuters.com/reuters/industrialsNews',
+          'http://feeds.reuters.com/reuters/USmediaDiversifiedNews',
+          'http://feeds.reuters.com/reuters/noncyclicalconsumergoodsNews',
+          'http://feeds.reuters.com/reuters/technologysectorNews',
+          'http://feeds.reuters.com/reuters/UStechnologyTelcomNews',
+          'http://feeds.reuters.com/reuters/telecomsectorNews',
+          'http://feeds.reuters.com/reuters/utilitiesNews']
 
-feedlist=['http://today.reuters.com/rss/topNews',
-          'http://today.reuters.com/rss/domesticNews',
-          'http://today.reuters.com/rss/worldNews',
-          'http://hosted.ap.org/lineups/TOPHEADS-rss_2.0.xml',
-          'http://hosted.ap.org/lineups/USHEADS-rss_2.0.xml',
-          'http://hosted.ap.org/lineups/WORLDHEADS-rss_2.0.xml',
-          'http://hosted.ap.org/lineups/POLITICSHEADS-rss_2.0.xml',
-          'http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml',
-          'http://www.nytimes.com/services/xml/rss/nyt/International.xml',
-          'http://news.google.com/?output=rss',
-          'http://feeds.salon.com/salon/news',
-          'http://www.foxnews.com/xmlfeed/rss/0,4313,0,00.rss',
-          'http://www.foxnews.com/xmlfeed/rss/0,4313,80,00.rss',
-          'http://www.foxnews.com/xmlfeed/rss/0,4313,81,00.rss',
-          'http://rss.cnn.com/rss/edition.rss',
-          'http://rss.cnn.com/rss/edition_world.rss',
-          'http://rss.cnn.com/rss/edition_us.rss']
+def stripHTML(article):
+    """Strips HTML and/or Markup tags from a given article. Function 
+    returns a string containing only the text elements of the original
+    article.
 
-def stripHTML(h):
-  p=''
-  s=0
-  for c in h:
-    if c=='<': s=1
-    elif c=='>':
-      s=0
-      p+=' '
-    elif s==0: p+=c
-  return p
+    """
+    # initialize an empty string which will be filled with text
+    raw_text = ''
+    # initialize a marker
+    s = 0
+
+    for char in article:
+        # mark start of HTML tag
+        if char == '<': 
+            s = 1
+        # mark end of HTML tag and add white space to raw_text
+        elif char == '>':
+            s = 0
+            raw_text += ' '
+        elif s == 0: 
+            raw_text += char
+    return raw_text
 
 
-def separatewords(text):
-  splitter=re.compile('\\W*')
-  return [s.lower() for s in splitter.split(text) if len(s)>3]
+def separateWords(text, min_length=3):
+    """Extracts separate words from s string of text. Function returns 
+    a list of individual words greater than some minimum word length.
 
-def getarticlewords():
-  allwords={}
-  articlewords=[]
-  articletitles=[]
-  ec=0
-  # Loop over every feed
-  for feed in feedlist:
-    f=feedparser.parse(feed)
+    """
+    # split the text into separate words 
+    split_text = re.compile('\\W*')
     
-    # Loop over every article
-    for e in f.entries:
-      # Ignore identical articles
-      if e.title in articletitles: continue
+    return [s.lower() for s in split_text.split(text) if len(s) > min_length]
+
+def get_articleWords(min_length=3):
+    """Function loops over all the news feeds, parses them with 
+    feedparser, strips out the HTML, and extracts the individual words
+    using the functions defined above. 
+
+    It keeps tracks of how many times each word is used overall, as 
+    well as how many times it is used in each individual article.
+
+    The function defines three variables:
+        - allwords: keeps a count of word usage across all the 
+                    different articles.
+        - articlewords: the counts of the words in each article. 
+        - articletitles: is a list of the titles of the articles.
+        
+    """
+    # define important variables
+    allWords      = {}
+    articleWords  = []
+    articleTitles = []
+
+    # define a counter
+    ec = 0
+
+    # Loop over every feed
+    for feed in feedlist:
+        tmp_feed = feedparser.parse(feed)
+    
+        # Loop over every article
+        for article in tmp_feed.entries:
+            # Ignore identical articles
+            if article.title in articleTitles: 
+                continue
       
-      # Extract the words
-      txt=e.title.encode('utf8')+stripHTML(e.description.encode('utf8'))
-      words=separatewords(txt)
-      articlewords.append({})
-      articletitles.append(e.title)
+            # Extract the words
+            tmp_txt = article.title.encode('utf8') + \
+                      stripHTML(article.description.encode('utf8'))
+                  
+            words = separateWords(tmp_txt, min_length)
+            articleWords.append({})
+            articleTitles.append(article.title)
       
-      # Increase the counts for this word in allwords and in articlewords
-      for word in words:
-        allwords.setdefault(word,0)
-        allwords[word]+=1
-        articlewords[ec].setdefault(word,0)
-        articlewords[ec][word]+=1
-      ec+=1
-  return allwords,articlewords,articletitles
+            # Increase the counts for this word in allWords and articleWords
+            for word in words:
+                allWords.setdefault(word, 0)
+                allWords[word] += 1
+                articleWords[ec].setdefault(word, 0)
+                articleWords[ec][word] += 1
+            ec += 1
+    return allWords, articleWords, articleTitles
 
-def makematrix(allw,articlew):
-  wordvec=[]
-  
-  # Only take words that are common but not too common
-  for w,c in allw.items():
-    if c>3 and c<len(articlew)*0.6:
-      wordvec.append(w) 
-  
-  # Create the word matrix
-  l1=[[(word in f and f[word] or 0) for word in wordvec] for f in articlew]
-  return l1,wordvec
+def makeMatrix(allw, articlew, min_num=3, max_percent=0.60):
+    """Now we have dictionaries of counts for the words in all articles,
+    as well as counts for each article, all of which has to converted 
+    into matrix that can be used in later analysis.  
 
-from numpy import *
+    First step is to create a list of words to be used as the columns 
+    of the matrix. To reduce the size of the overall matrix I will 
+    eliminate words that appear in only a couple of articles and also 
+    those that appear in too many articles.
 
-def showfeatures(w,h,titles,wordvec,out='features.txt'): 
-  outfile=file(out,'w')  
-  pc,wc=shape(h)
-  toppatterns=[[] for i in range(len(titles))]
-  patternnames=[]
-  
-  # Loop over all the features
-  for i in range(pc):
-    slist=[]
-    # Create a list of words and their weights
-    for j in range(wc):
-      slist.append((h[i,j],wordvec[j]))
-    # Reverse sort the word list
-    slist.sort()
-    slist.reverse()
+    To start, try only including words that appear in more than 
+    min_num of articles, but fewer than some max_percent of the total
+    number of articles.
     
-    # Print the first six elements
-    n=[s[1] for s in slist[0:6]]
-    outfile.write(str(n)+'\n')
-    patternnames.append(n)
+    """
+    wordVec = []
+  
+    # Only take words that are common but not too common
+    for word, count in allw.items():
+        if count > min_num and count < len(articlew) * max_percent: 
+            wordVec.append(word) 
+  
+    # Create the word matrix
+    l1 = [[(word in f and f[word] or 0) for word in wordVec] for f in articlew]
+    return l1, wordVec
+
+
+
+def showFeatures(w, h, titles, wordvec, out='features.txt'):
+    """Exactly how you view results of NNMF is a bit complicated. Every
+    feature in the features matrix has a weighting that indicates how
+    strongly each word applies to that feature, so you might try 
+    displaying the top five or ten words in each feature to see what 
+    the most important words are in each feature.
+
+    The equivalent column in the weights matrix tells you how much 
+    this particular feature applies to each of the articles, so it is 
+    often interesting to show the top few articles and see how strongly
+    a particular feature applies to them.
+
+    This function loops over each word of the features and creates a 
+    list of all the word weights and words from the word vector.  It 
+    sorts the list so that the most heavily weighted words for that 
+    feature appear at the start of the list, and then prints the first
+    10 of these words. Hopefully this gives some idea of the theme 
+    represented by a particular feature.
+
+    After displaying each feature, the function loops over the article
+    titles and sorts them according to their value in the weights 
+    matrix for that article and feature. It then prints the titles of 
+    3 articles most strongly associated with that feature, along with 
+    the value from the weights matrix. Sometimes a feature will be 
+    important for several related articles, and that sometimes a 
+    feature will only show up prominately in a single article.
     
-    # Create a list of articles for this feature
-    flist=[]
+    """
+    outfile = file(out,'w')  
+    pc, wc = np.shape(h)
+
+    # create storage containers for sorted features
+    toppatterns=[[] for i in range(len(titles))]
+    patternnames=[]
+  
+    # Loop over all the features
+    for i in range(pc):
+        slist=[]
+        # Create a list of words and their weights
+        for j in range(wc):
+            slist.append((h[i,j], wordvec[j]))
+        # Reverse sort the word list
+        slist.sort()
+        slist.reverse()
+    
+        # Print the first ten elements
+        n = [s[1] for s in slist[0:10]]
+        outfile.write(str(n) + '\n')
+        patternnames.append(n)
+    
+        # Create a list of articles for this feature
+        flist = []
+        for j in range(len(titles)):
+            # Add the article with its weight
+            flist.append((w[j, i], titles[j]))
+            toppatterns[j].append((w[j, i], i, titles[j]))
+    
+        # Reverse sort the list
+        flist.sort()
+        flist.reverse()
+    
+        # Show the top 3 articles
+        for f in flist[0:3]:
+            outfile.write(str(f)+'\n')
+        outfile.write('\n')
+
+    outfile.close()
+
+    # Return the pattern names for later use
+    return toppatterns, patternnames
+
+def showArticles(titles, toppatterns ,patternnames, out='articles.txt'):
+    """Alternative way to display the data is to show each article and 
+    the top 3 features that apply to it.  This allows you to see if an 
+    article consists of equal amounts of several themes of a single 
+    strong theme.
+
+    """
+    outfile = file(out,'w')  
+  
+    # Loop over all the articles
     for j in range(len(titles)):
-      # Add the article with its weight
-      flist.append((w[j,i],titles[j]))
-      toppatterns[j].append((w[j,i],i,titles[j]))
+        outfile.write(titles[j].encode('utf8') + '\n')
     
-    # Reverse sort the list
-    flist.sort()
-    flist.reverse()
+        # Get the top features for this article and reverse sort them
+        toppatterns[j].sort()
+        toppatterns[j].reverse()
     
-    # Show the top 3 articles
-    for f in flist[0:3]:
-      outfile.write(str(f)+'\n')
-    outfile.write('\n')
-
-  outfile.close()
-  # Return the pattern names for later use
-  return toppatterns,patternnames
-
-def showarticles(titles,toppatterns,patternnames,out='articles.txt'):
-  outfile=file(out,'w')  
-  
-  # Loop over all the articles
-  for j in range(len(titles)):
-    outfile.write(titles[j].encode('utf8')+'\n')
+        # Print the top three patterns
+        for i in range(3):
+            outfile.write(str(toppatterns[j][i][0]) + ' ' + \
+                          str(patternnames[toppatterns[j][i][1]])+'\n')
+        outfile.write('\n')
     
-    # Get the top features for this article and
-    # reverse sort them
-    toppatterns[j].sort()
-    toppatterns[j].reverse()
-    
-    # Print the top three patterns
-    for i in range(3):
-      outfile.write(str(toppatterns[j][i][0])+' '+
-                    str(patternnames[toppatterns[j][i][1]])+'\n')
-    outfile.write('\n')
-    
-  outfile.close()
+    outfile.close()
--- chapter10/nnmf.py
+++ chapter10/nnmf.py
@@ -1,43 +1,96 @@
-from numpy import *
+""" The Non-negative matrix factorization algorithm tries to reconstruct 
+the articles matrix as closely as possible by calculating the best 
+features and weight matrices. 
 
-def difcost(a,b):
-  dif=0
-  for i in range(shape(a)[0]):
-    for j in range(shape(a)[1]):
-      # Euclidean Distance
-      dif+=pow(a[i,j]-b[i,j],2)
-  return dif
+The features matrix: features matrix has a row for each feature and a 
+column for each word. Thus each feature is effectivly a list of word
+weights. The ij-entry in the features matrix indicates how important 
+that word is for a given feature. Each feature should represent a 
+theme tha emerged from a set of articles. Since each feature is a 
+combination of different words, it should be clear that reconstructing
+the articles matrix is a matter of combining rows from the features 
+matrix in different amounts (i.e., weights).
 
-def factorize(v,pc=10,iter=50):
-  ic=shape(v)[0]
-  fc=shape(v)[1]
+The weights matrix: the weights matrix maps the features matrix to the
+articles matrix. Each row is an article, and each column is a feature.
+The ij-entry indicates how strongly a given feature is present in each
+article.
 
-  # Initialize the weight and feature matrices with random values
-  w=matrix([[random.random() for j in range(pc)] for i in range(ic)])
-  h=matrix([[random.random() for i in range(fc)] for i in range(pc)])
+While in principle, the NNMF algorithm can reconstruct the articles 
+matrix exactly, in practice this is rarely possible. The idea is to 
+try and reproduce/reconstruct the articles matrix from the features 
+matrix as closely as possible by choosing appropriate weights for the 
+features.
 
-  # Perform operation a maximum of iter times
-  for i in range(iter):
-    wh=w*h
+"""
+
+import numpy as np
+
+def costFunction(A, B):
+    """Need a way to measure just how close the result is. This function 
+    computes the sum of squared differences between two matrices, A and 
+    B, which is used as the measure of cost.
+
+    """
+    return np.sum((A - B)**2)
+
+def factorize(v, pc=10, iter=50, seed=42):
+    """The update rules generates four new update matrices.  In these 
+    descriptions, the original articles matrix is referred to as the 
+    data matrix:
     
-    # Calculate the current difference
-    cost=difcost(v,wh)
+    - hn: The transposed weight matrix multiplied by the data matrix.
+    - hd: The transposed weight matrix multiplied by itself, multiplied
+          by the features matrix.
+    - wn: The data matrix multiplied by the transposed features matrix.
+    - wd: The weights matrix multiplied by the features matrix 
+          multiplied by the transposed features matrix.
+
+    For detailed derivation of the multiplicative update rules see:
+
+        http://hebb.mit.edu/people/seung/papers/nmfconverge.pdf
     
-    if i%10==0: print cost
+    Function requires user to specify, pc, the number of features you 
+    want to find.  In some cases, you'll know how many features you 
+    expect to find in the data, but in other cases, you'll have no 
+    idea how many to specify. Generally no way to automatically 
+    determine the correct number of components, but experimentation 
+    can help find the appropriate range.
     
-    # Terminate if the matrix has been fully factorized
-    if cost==0: break
+    """
+    ic = np.shape(v)[0]
+    fc = np.shape(v)[1]
+
+    # Initialize the weight and feature matrices with random values
+    np.random.seed(seed)
+    w = np.random.random((ic, pc))
+    h = np.random.random((pc, fc))
+
+    # Perform operation a maximum of iter times
+    for i in range(iter):
+        wh = w.dot(h)
     
-    # Update feature matrix
-    hn=(transpose(w)*v)
-    hd=(transpose(w)*w*h)
+        # Calculate the current difference
+        cost = costFunction(v, wh)
+
+        # keep trak of progess
+        if i%10 == 0: 
+            print cost
+    
+        # Terminate if the matrix has been fully factorized
+        if cost == 0: 
+            break
+    
+        # Update features matrix
+        hn = (w.T).dot(v)
+        hd = ((w.T).dot(w)).dot(h) + 0.000000001 
   
-    h=matrix(array(h)*array(hn)/array(hd))
+        h  = (h * hn) / hd
 
-    # Update weights matrix
-    wn=(v*transpose(h))
-    wd=(w*h*transpose(h))
+        # Update weights matrix
+        wn = v.dot(h.T)
+        wd = w.dot(h.dot(h.T)) + 0.000000001
 
-    w=matrix(array(w)*array(wn)/array(wd))  
+        w  = (w * wn) / wd   
     
-  return w,h
+    return w, h
