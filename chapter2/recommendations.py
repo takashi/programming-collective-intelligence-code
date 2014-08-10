78f2854fa88c578757e6cb23754f1c73ea87f627
Takashi Nakagawa <tak1240@gmail.com>
2014-08-10 19:01:19 +0900

    remove white space

--- chapter2/recommendations.py
+++ chapter2/recommendations.py
@@ -1,19 +1,19 @@
 # A dictionary of movie critics and their ratings of a small
 # set of movies
 critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
- 'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5, 
+ 'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5,
  'The Night Listener': 3.0},
-'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5, 
- 'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0, 
- 'You, Me and Dupree': 3.5}, 
+'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5,
+ 'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0,
+ 'You, Me and Dupree': 3.5},
 'Michael Phillips': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0,
  'Superman Returns': 3.5, 'The Night Listener': 4.0},
 'Claudia Puig': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0,
- 'The Night Listener': 4.5, 'Superman Returns': 4.0, 
+ 'The Night Listener': 4.5, 'Superman Returns': 4.0,
  'You, Me and Dupree': 2.5},
-'Mick LaSalle': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 
+'Mick LaSalle': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
  'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Listener': 3.0,
- 'You, Me and Dupree': 2.0}, 
+ 'You, Me and Dupree': 2.0},
 'Jack Matthews': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
  'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
 'Toby': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0}}
@@ -25,14 +25,14 @@ from math import sqrt
 def sim_distance(prefs,person1,person2):
   # Get the list of shared_items
   si={}
-  for item in prefs[person1]: 
+  for item in prefs[person1]:
     if item in prefs[person2]: si[item]=1
 
   # if they have no ratings in common, return 0
   if len(si)==0: return 0
 
   # Add up the squares of all the differences
-  sum_of_squares=sum([pow(prefs[person1][item]-prefs[person2][item],2) 
+  sum_of_squares=sum([pow(prefs[person1][item]-prefs[person2][item],2)
                       for item in prefs[person1] if item in prefs[person2]])
 
   return 1/(1+sum_of_squares)
@@ -41,7 +41,7 @@ def sim_distance(prefs,person1,person2):
 def sim_pearson(prefs,p1,p2):
   # Get the list of mutually rated items
   si={}
-  for item in prefs[p1]: 
+  for item in prefs[p1]:
     if item in prefs[p2]: si[item]=1
 
   # if they are no ratings in common, return 0
@@ -49,18 +49,18 @@ def sim_pearson(prefs,p1,p2):
 
   # Sum calculations
   n=len(si)
-  
+
   # Sums of all the preferences
   sum1=sum([prefs[p1][it] for it in si])
   sum2=sum([prefs[p2][it] for it in si])
-  
+
   # Sums of the squares
   sum1Sq=sum([pow(prefs[p1][it],2) for it in si])
-  sum2Sq=sum([pow(prefs[p2][it],2) for it in si])	
-  
+  sum2Sq=sum([pow(prefs[p2][it],2) for it in si])
+
   # Sum of the products
   pSum=sum([prefs[p1][it]*prefs[p2][it] for it in si])
-  
+
   # Calculate r (Pearson score)
   num=pSum-(sum1*sum2/n)
   den=sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
@@ -70,10 +70,10 @@ def sim_pearson(prefs,p1,p2):
 
   return r
 
-# Returns the best matches for person from the prefs dictionary. 
+# Returns the best matches for person from the prefs dictionary.
 # Number of results and similarity function are optional params.
 def topMatches(prefs,person,n=5,similarity=sim_pearson):
-  scores=[(similarity(prefs,person,other),other) 
+  scores=[(similarity(prefs,person,other),other)
                   for other in prefs if other!=person]
   scores.sort()
   scores.reverse()
@@ -92,7 +92,7 @@ def getRecommendations(prefs,person,similarity=sim_pearson):
     # ignore scores of zero or lower
     if sim<=0: continue
     for item in prefs[other]:
-	    
+
       # only score movies I haven't seen yet
       if item not in prefs[person] or prefs[person][item]==0:
         # Similarity * Score
@@ -115,7 +115,7 @@ def transformPrefs(prefs):
   for person in prefs:
     for item in prefs[person]:
       result.setdefault(item,{})
-      
+
       # Flip item and person
       result[item][person]=prefs[person][item]
   return result
@@ -164,13 +164,13 @@ def getRecommendedItems(prefs,itemMatch,user):
   rankings.reverse( )
   return rankings
 
-def loadMovieLens(path='/data/movielens'):
+def loadMovieLens(path='./data/movielens'):
   # Get movie titles
   movies={}
   for line in open(path+'/u.item'):
     (id,title)=line.split('|')[0:2]
     movies[id]=title
-  
+
   # Load data
   prefs={}
   for line in open(path+'/u.data'):
